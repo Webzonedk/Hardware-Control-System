c10d930d85702a636aa9c525bdd8482ccaea3043
Kent Pedersen <webzone@kpdesign.dk>
2020-10-29 12:45:20 +0100

    adda db strings

--- Startup.cs
+++ Startup.cs
@@ -29,7 +29,7 @@ namespace RedCrossItCheckingSystem
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                    Configuration.GetConnectionString("UserContext")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
