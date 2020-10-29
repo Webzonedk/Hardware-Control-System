bcd2572f04cacffe93e7991d8395a956920dea0a
Kent Pedersen <webzone@kpdesign.dk>
2020-10-29 13:17:22 +0100

    secret

--- Startup.cs
+++ Startup.cs
@@ -30,6 +30,7 @@ namespace RedCrossItCheckingSystem
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("UserContext")));
+
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
