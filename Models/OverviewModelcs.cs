3bf84f1370bb2af3010f0cfd9d58062cd6bb84c6
Andreas Boll Nielsen <andreas@boll-nielsen.dk>
2020-11-24 12:38:50 +0100

    garbage!

---
+++ Models/OverviewModelcs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedCrossItCheckingSystem.Models
+{
+    public class OverviewModelcs
+    {
+        List<DataContainer> containers;
+
+    }
+}
